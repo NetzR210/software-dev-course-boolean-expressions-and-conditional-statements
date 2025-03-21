804f9b815fee80b022ca827d7201a78705fb8a3a
Rachael <rachaelnetz618@duck.com>
2025-03-21 11:44:45 -0500

    Add initial code

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -50,3 +50,39 @@ Add Customization and expand the game:
   - Use nested conditionals and logical operators to create complex outcomes.
 
 */
+
+const readline = require('readline-sync');
+
+const hasTorch = true;
+const hasMap = false;
+const hasSword = true;
+const hasCompass = false;
+
+console.log("You see two paths: one leads to the mountains, the other to the village.");
+const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+
+if (choice === "mountains") {
+  if (hasTorch) {
+    console.log("You safely navigate through the dark mountains.");
+    if (hasSword) {
+      console.log("You fend off a wild beast with your sword and discover treasure!");
+    } else {
+      console.log("A wild beast attacks you! You barely escape.");
+    }
+  } else {
+    console.log("It's too dark to proceed. You decide to turn back.");
+  }
+} else if (choice === "village") {
+  if (hasMap || hasCompass) {
+    console.log("You find your way to the village easily.");
+    if (hasTorch) {
+      console.log("You use your torch to help the villagers find a lost child. They reward you with food.");
+    } else {
+      console.log("You enjoy a peaceful evening in the village.");
+    }
+  } else {
+    console.log("You get lost on the way to the village.");
+  }
+} else {
+  console.log("You hesitate and end up wandering aimlessly.");
+}
